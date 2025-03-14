4f234069c16e038e32fae5f4586bdc49efb48e25
Jeremy Angers <swat.angers@gmail.com>
2025-03-13 22:05:47 -0400

    Changed the first sentence

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from buffer import *
 # We define the main program
 def main():
     print("Hello World!")
-    print("I'm a easy programme that create a buffer and show it to you")
+    print("I'm a programme that create a array and show it to you")
     buffer = create_buffer()
     show_buffer(buffer)
     
