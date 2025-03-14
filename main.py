511e315091e5384ec39743ecede4b3b4f38f8088
Jeremy Angers <swat.angers@gmail.com>
2025-03-13 21:59:01 -0400

    Changed the sentence in the main routine

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from buffer import *
 # We define the main program
 def main():
     print("Hello World!")
-    print("I'm a easy programme that create a buffer and show it to you")
+    print("I'm a easy programme that create a buffer that you can see in your terminal")
     buffer = create_buffer()
     show_buffer(buffer)
     
