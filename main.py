c266e61bdb90d37bfa62d02f6387ae95b2f74fe4
Jeremy Angers <swat.angers@gmail.com>
2025-03-13 22:06:55 -0400

    Chenged the main sentence but with an author way

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from buffer import *
 # We define the main program
 def main():
     print("Hello World!")
-    print("I'm a easy programme that create a buffer that you can see in your terminal")
+    print("I'm a easy program that create a list")
     buffer = create_buffer()
     show_buffer(buffer)
     
